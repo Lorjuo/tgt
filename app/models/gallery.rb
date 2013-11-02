ed1f3d577105950140ba7868b7650202146bc6b1
Julien Seitz <julien.seitz@gmail.com>
2013-11-02 15:42:09 +0100

    minor changes

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,10 +1,13 @@
 class Gallery < ActiveRecord::Base
 
   # Associations
-  has_many :images, :as => :attachable, :class_name => '::Image'
+  has_many :images, :as => :attachable, :class_name => '::Image', dependent: :destroy
   belongs_to :preview_image, :class_name => '::Image'
 
   accepts_nested_attributes_for :images, allow_destroy: true
+
+  # Validation
+  validates :name, :presence => true
 
   def get_preview_image
     self.preview_image || self.images.first
