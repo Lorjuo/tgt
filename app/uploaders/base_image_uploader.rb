df7c4546cb06616cf9add6bff082da1fa2622c9c
Julien Seitz <julien.seitz@gmail.com>
2014-03-24 17:39:36 +0100

    improved image compression

--- app/uploaders/base_image_uploader.rb
+++ app/uploaders/base_image_uploader.rb
@@ -5,10 +5,10 @@ class BaseImageUploader < BaseUploader
 
   after :store, :unlink_original
   
-  process quality: 90
+  process quality: 80 #https://github.com/petedoyle/imagemagick-quality-tests
 
   process resize_to_fit: [800, 600]
-
+  process :strip
   process :set_content_type
 
   def extension_white_list
@@ -17,6 +17,16 @@ class BaseImageUploader < BaseUploader
 
   def unlink_original(file)
     File.delete if version_name.blank?
+  end
+
+  # http://stackoverflow.com/questions/4753408/how-to-remove-exif-camera-data-from-image-with-carrierwave
+  # Strip EXIF Metadata to get smaller file sizes
+  def strip
+    manipulate! do |img|
+      img.strip
+      img = yield(img) if block_given?
+      img
+    end
   end
 
 end
