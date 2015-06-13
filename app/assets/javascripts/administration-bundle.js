b0ab6b21f353d9e38842c03ff8763789ad09fbc9
Julien Seitz <julien.seitz@gmail.com>
2015-06-13 11:08:29 +0200

    administration-bundle refactored

--- app/assets/javascripts/administration-bundle.js
+++ app/assets/javascripts/administration-bundle.js
@@ -5,6 +5,67 @@
 //= require crop
 //= require crop_loader
 //
+//
+//# Colorpicker
+//= require bootstrap-colorpicker
+//= require colorpicker_loader
+//
+//# Forked Datetimepicker
+//# require bootstrap-datetimepicker
+//= require bootstrap-datetimepicker/core
+//# require bootstrap-datetimepicker/pickers
+//= require bootstrap-datetimepicker/locales/bootstrap-datetimepicker.de.js
+//= require datetimepicker_loader
+//
+// 
+//# File Upload
+//# require jquery-fileupload
+//# require jquery-fileupload/basic
+//
+//# Fileupload needs to be in the right order
+//# https://github.com/blueimp/jQuery-File-Upload/wiki/Client-side-Image-Resizing
+//# http://stackoverflow.com/questions/19096946/how-to-resize-images-client-side-using-jquery-file-upload
+//# require jquery-fileupload/vendor/jquery.ui.widget
+//# require jquery-fileupload/vendor/load-image
+//# require jquery-fileupload/vendor/canvas-to-blob
+//# require jquery-fileupload/vendor/tmpl
+//# require jquery-fileupload/jquery.iframe-transport
+//# require jquery-fileupload/jquery.fileupload
+//# require jquery-fileupload/jquery.fileupload-ui
+//# require jquery-fileupload/jquery.fileupload-process
+//# require jquery-fileupload/jquery.fileupload-validate
+//# require jquery-fileupload/jquery.fileupload-image
+//
+//= require jquery-fileupload/vendor/jquery.ui.widget
+//= require jquery-fileupload/vendor/tmpl
+//= require jquery-fileupload/vendor/load-image.all.min
+//= require jquery-fileupload/vendor/canvas-to-blob
+//= require jquery-fileupload/jquery.iframe-transport
+//= require jquery-fileupload/jquery.fileupload
+//= require jquery-fileupload/jquery.fileupload-process
+//= require jquery-fileupload/jquery.fileupload-image
+//# require jquery-fileupload/jquery.fileupload-audio
+//# require jquery-fileupload/jquery.fileupload-video
+//= require jquery-fileupload/jquery.fileupload-validate
+//= require jquery-fileupload/jquery.fileupload-ui
+//= require jquery-fileupload/locale
+//# require jquery-fileupload/jquery.fileupload-angular
+//# require jquery-fileupload/jquery.fileupload-jquery-ui
+//= require jquery-fileupload/cors/jquery.postmessage-transport
+//= require jquery-fileupload/cors/jquery.xdr-transport
+//
+//= require fileupload
+//
+//# File Upload Button
+//= require fileupload_button
+//
+//# Sortable Tree
+//= require jquery.ui.nestedSortable
+//= require sortable_tree/initializer
+//
+//
+//
+//
 //# WYSIWYG
 //= require tinymce-jquery
 //= require administration/tinymce_defaults
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -35,70 +35,19 @@
 //#ZeroClipBoardDisabled:
 // require zeroclipboard
 //
-//# Colorpicker
-//= require bootstrap-colorpicker
-//= require colorpicker_loader
-//
-//# Forked Datetimepicker
-//# require bootstrap-datetimepicker
-//= require bootstrap-datetimepicker/core
-//# require bootstrap-datetimepicker/pickers
-//= require bootstrap-datetimepicker/locales/bootstrap-datetimepicker.de.js
-//= require datetimepicker_loader
-//
 //# EqualHeights # TODO: Maybe remove this
 //= require equal_heights_loader
 //
 //# Google Maps # TODO: Maybe remove this
 //#Gmaps4RailsDisabled:
 // require gmaps/google
-// 
-//# File Upload
-//# require jquery-fileupload
-//# require jquery-fileupload/basic
-//
-//# Fileupload needs to be in the right order
-//# https://github.com/blueimp/jQuery-File-Upload/wiki/Client-side-Image-Resizing
-//# http://stackoverflow.com/questions/19096946/how-to-resize-images-client-side-using-jquery-file-upload
-//# require jquery-fileupload/vendor/jquery.ui.widget
-//# require jquery-fileupload/vendor/load-image
-//# require jquery-fileupload/vendor/canvas-to-blob
-//# require jquery-fileupload/vendor/tmpl
-//# require jquery-fileupload/jquery.iframe-transport
-//# require jquery-fileupload/jquery.fileupload
-//# require jquery-fileupload/jquery.fileupload-ui
-//# require jquery-fileupload/jquery.fileupload-process
-//# require jquery-fileupload/jquery.fileupload-validate
-//# require jquery-fileupload/jquery.fileupload-image
-//
-//= require jquery-fileupload/vendor/jquery.ui.widget
-//= require jquery-fileupload/vendor/tmpl
-//= require jquery-fileupload/vendor/load-image.all.min
-//= require jquery-fileupload/vendor/canvas-to-blob
-//= require jquery-fileupload/jquery.iframe-transport
-//= require jquery-fileupload/jquery.fileupload
-//= require jquery-fileupload/jquery.fileupload-process
-//= require jquery-fileupload/jquery.fileupload-image
-//# require jquery-fileupload/jquery.fileupload-audio
-//# require jquery-fileupload/jquery.fileupload-video
-//= require jquery-fileupload/jquery.fileupload-validate
-//= require jquery-fileupload/jquery.fileupload-ui
-//= require jquery-fileupload/locale
-//# require jquery-fileupload/jquery.fileupload-angular
-//# require jquery-fileupload/jquery.fileupload-jquery-ui
-//= require jquery-fileupload/cors/jquery.postmessage-transport
-//= require jquery-fileupload/cors/jquery.xdr-transport
-//
-//= require fileupload
-//
-//= require jquery
-// require jquery.jcrop
-//
-//# File Upload Button
-//= require fileupload_button
 //
 //# Forms
 //= require ajax_forms
+//
+//# Nested Forms
+//= require cocoon
+//= require nested_forms_loader
 // 
 //# Input Elements
 //# Chosen
@@ -120,10 +69,6 @@
 //= require navigation
 //= require links
 //
-//# Nested Forms
-//= require cocoon
-//= require nested_forms_loader
-//
 //# Pagination
 //= require endless_page
 //
@@ -132,18 +77,6 @@
 //
 //# Select
 //= require select
-//# Sortable Tree
-//= require jquery.ui.nestedSortable
-//= require sortable_tree/initializer
-//
-//# Table
-//= require dataTables/jquery.dataTables
-//= require dataTables/bootstrap/3/jquery.dataTables.bootstrap
-//= require dataTables/extras/dataTables.responsive
-//# require dataTables/jquery.dataTables
-//# require dataTables/jquery.dataTables.bootstrap3
-//# require dataTables/jquery.dataTables.responsive
-//= require datatables
 //
 //# Underscore js
 //=require underscore
