77881a1faf1ded4c52313c1fbfa02347442f741b
Julien Seitz <julien.seitz@gmail.com>
2015-06-13 13:15:06 +0200

    minor design changes

--- app/assets/javascripts/administration/tinymce_defaults.js
+++ app/assets/javascripts/administration/tinymce_defaults.js
@@ -143,6 +143,9 @@ window.tinymceDefaults = {
             }]
         },
     ],
+
+    fontsize_formats: "8pt 10pt 12pt 14pt 18pt 24pt 36pt",
+
     // TODO: Removeformat does not work for table classes
     removeformat: [{
         selector: 'table',
