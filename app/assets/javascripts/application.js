37aaf3aa19de0f8be08ee259897f97d03a30e454
Julien Seitz <julien.seitz@gmail.com>
2015-10-27 00:09:34 +0100

    removed deferred assets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,7 +19,83 @@
 //# require jquery.ui.all
 //#= require jquery_ujs
 //
+////# require holder
 //
+//# Bootstrap
+//= require bootstrap-sprockets
+//# Bootstrap Javascript
+// require bootstrap/scrollspy
+// require bootstrap/modal
+//= require bootstrap/dropdown
+//# Loaders
+//= require tooltip_loader
+//= require bootstrap_alert
+//
+//# Clipboard # TODO: Check if this is needed
+//#ZeroClipBoardDisabled:
+// require zeroclipboard
+//
+//# EqualHeights # TODO: Maybe remove this
+//# require equal_heights_loader
+//
+//# Grid # Has to be some of the last scripts to include, because especially chosen can change the page adjustments
+//# require jquery.equalheights
+//
+//# Google Maps # TODO: Maybe remove this
+//#Gmaps4RailsDisabled:
+// require gmaps/google
+//
+//# Forms
+//= require ajax_forms
+//
+//# Nested Forms
+//= require cocoon
+//= require nested_forms_loader
+// 
+//# Input Elements
+//# Chosen
+//= require chosen-jquery
+//= require chosen_loader
+//= require jquery_popup_uploader
+//
+//# Images
+//= require fancybox_loader
+//= require fancybox
+//= require jquery.mousewheel-3.0.6.pack
+//= require preview_cycle
+//= require cycle2_loader
+//= require jquery.cycle2
+// require jquery.cycle2.caption2.js
+//# At the moment this is not needed because it does not work with turbolinks
+//
+//# Navigation
+//= require navigation
+//= require links
+//
+//# Pagination
+//= require endless_page
+//
+//# Seat Reservation
+//= require seatReservation
+//
+//# Select
+//= require select
+//
+//
+//# Underscore js
+//=require underscore
+//# at the moment needed for jquery dataTables responsive and gmaps4rails
+//
+// require_tree .
+// require_directory .
+// 
+//# Specific Pages
+//= require home
+//= require messages
+//= require schedule
+//
+//# Swype
+//= require jquery.touchSwipe.js
 //
 //# Has to be the last (https://github.com/kossnocorp/jquery.turbolinks)
 //= require turbolinks
