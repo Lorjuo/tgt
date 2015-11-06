65791dcd9981f9ab84db9e556490a5ff067df019
Julien Seitz <julien.seitz@gmail.com>
2015-11-06 21:57:19 +0100

    minor changes

--- app/assets/javascripts/_datatables-bundle.js
+++ app/assets/javascripts/_datatables-bundle.js
@@ -5,6 +5,3 @@
 //# require dataTables/jquery.dataTables
 //# require dataTables/jquery.dataTables.bootstrap3
 //# require dataTables/jquery.dataTables.responsive
-//<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/r/bs/jq-2.1.4,dt-1.10.9,r-1.0.7/datatables.min.css"/>
- 
-//<script type="text/javascript" src="https://cdn.datatables.net/r/bs/jq-2.1.4,dt-1.10.9,r-1.0.7/datatables.min.js"></script>
